5b6871d908e1fdf9303847f8b85b24008111abf6
ajaysharma1997 <45599664+ajaysharma1997@users.noreply.github.com>
2022-04-26 16:47:35 +0530

    Misaligned class updated..

--- misaligned/misaligned.java
+++ misaligned/misaligned.java
@@ -1,15 +1,24 @@
+package misaligned;
+
 public class Misaligned {
     static int printColorMap() {
         String majorColors[] = {"White", "Red", "Black", "Yellow", "Violet"};
         String minorColors[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
-        int i = 0, j = 0;
-        for(i = 0; i < 5; i++) {
-            for(j = 0; j < 5; j++) {
-                System.out.printf("%d | %s | %s\n", i * 5 + j, majorColors[i], minorColors[i]);
+        int majorColorIndex = 0, minorColorIndex = 0;
+        for(majorColorIndex = 0; majorColorIndex < 5; majorColorIndex++) {
+            for(minorColorIndex = 0; minorColorIndex < 5; minorColorIndex++) {
+            	assert(getPairNumberFromColor(majorColorIndex, minorColorIndex) == majorColorIndex * 5 + minorColorIndex);
+                System.out.printf("%d | %s | %s\n", majorColorIndex * 5 + minorColorIndex, majorColors[majorColorIndex], minorColors[majorColorIndex]);
             }
         }
-        return i * j;
+        return majorColorIndex * minorColorIndex;
     }
+    
+    public static int getPairNumberFromColor(int majorColorIndex, int minorColorIndex) {
+        String minorColors[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+        return majorColorIndex * minorColors.length + minorColorIndex + 1;
+    }
+    
     public static void main(String[] args) { 
         int result = printColorMap();
         assert(result == 25);
